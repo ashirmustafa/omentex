39d07ed3596dee92dcf51bba28c9573eae7d5172
Ashir Mustafa <muhammadashirone@gmail.com>
2024-08-24 13:07:36 +0500

    not-found.tsx textColor fix

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -6,12 +6,12 @@ const NotFound = () => {
   const router = useRouter();
   return (
     <>
-      <div className="h-screen  bg-primary flex flex-col items-center justify-center text-se text-3xl">
-        <h1 className="md:text-8xl text-2xl font-semibold pb-10">OMENTEX</h1>
+      <div className="h-screen  bg-primary flex flex-col items-center justify-center text-secondary text-3xl">
+        <h1 className="md:text-8xl text-2xl font-semibold pb-10 ">OMENTEX</h1>
         <h1 className="font-thin pb-3">404: Page not Found</h1>
         <div
           className="text-sm cursor-pointer font-bold underline"
-          onClick={() => router.push("/")}
+          onClick={() => router.back()}
         >
           Click here to return
         </div>
