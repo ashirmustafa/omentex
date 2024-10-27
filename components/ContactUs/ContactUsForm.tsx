f670c1593dde07627ac58a5eb3943df8df1344b9
Phojiii <qasimbilal19@gmail.com>
2024-10-27 14:14:50 +0500

    Changed the color of stack, fixed the desing of contact page

--- components/ContactUs/ContactUsForm.tsx
+++ components/ContactUs/ContactUsForm.tsx
@@ -7,7 +7,7 @@ interface InputFieldProps {
 
 const InputField: React.FC<InputFieldProps> = ({ label, placeHolder }) => (
   <div className="flex flex-col w-full">
-    <label htmlFor="" className="text-secondary">
+    <label htmlFor="" className="text-primary">
       {label}
     </label>
     <input
@@ -15,7 +15,7 @@ const InputField: React.FC<InputFieldProps> = ({ label, placeHolder }) => (
       name=""
       id=""
       placeholder={placeHolder}
-      className="bg-white placeholder:text-gray-700 px-4 py-2 border-gray-400 outline-none w-full"
+      className="bg-primary placeholder:text-gray-400 px-4 py-2 border-gray-400 outline-none w-full text-secondary"
     />
   </div>
 );
@@ -24,35 +24,35 @@ const ContactUsForm = () => {
   return (
     <div
       id="contact-form"
-      className="max-w-[1760px] w-full mx-auto lg:px-0 px-5 bg-secondary"
+      className="max-w-[1200px] w-full mx-auto lg:px-0 px-5"
     >
       <div className="flex gap-10 py-20 lg:flex-nowrap flex-wrap items-center">
         {/* left */}
         <div className="lg:max-w-[40%] max-w-full w-full">
-          <h1 className="2xl:text-6xl xl:text-4xl lg:text-2xl md:text-xl text-lg text-primary font-semibold">
+          <h1 className="2xl:text-6xl xl:text-4xl lg:text-2xl md:text-xl text-lg text-secondary font-semibold">
             We are here to help
           </h1>
-          <p className="lg:text-lg md:text-base text-sm lg:pt-4 pt-1 font-medium">
+          <p className="lg:text-lg md:text-base text-sm lg:pt-4 pt-1 font-medium text-secondary">
             {`Let's get this conversation started. Tell us a bit about yourself
             and we'll get in touch as soon as we can.`}
           </p>
 
-          <h1 className="lg:text-2xl md:text-xl text-lg font-semibold lg:pt-6 pt-3">
+          <h1 className="lg:text-2xl md:text-xl text-lg font-semibold lg:pt-6 pt-3 text-secondary">
             Looking to learn about careers at OMENTEX?
           </h1>
-          <p className="lg:text-lg md:text-base text-sm">
+          <p className="lg:text-lg md:text-base text-sm text-secondary">
             We do not accept applications via this form. To apply for roles,
             please review our portal here.
           </p>
           <div className="flex lg:justify-start justify-center">
-            <button className="bg-primary text-secondary px-5 py-2 hover:bg-secondary hover:text-primary border border-primary mt-8 transition-colors">
+            <button className="bg-secondary text-primary px-5 py-2 hover:bg-primary hover:text-secondary border border-secondary mt-8 transition-colors rounded-xl">
               Apply Now
             </button>
           </div>
         </div>
         {/* right */}
-        <div className="lg:max-w-[60%] max-w-full w-full bg-primary px-5">
-          <h1 className="lg:text-3xl md:text-xl text-lg font-semibold lg:pt-6 pt-3 text-secondary">
+        <div className="lg:max-w-[60%] max-w-full w-full bg-secondary px-5 rounded-xl">
+          <h1 className="lg:text-3xl md:text-xl text-lg font-semibold lg:pt-6 pt-3 text-primary">
             Contact Us
           </h1>
 
@@ -80,13 +80,13 @@ const ContactUsForm = () => {
               </div>
               <div className="flex justify-between lg:gap-9 lg:flex-nowrap flex-wrap">
                 <div className="flex flex-col w-full">
-                  <label className="text-secondary">Message *</label>
-                  <textarea placeholder="Enter Your Message" className="bg-white placeholder:text-gray-700 px-4 py-2 border-gray-400 outline-none w-full"></textarea>
+                  <label className="text-primary">Message *</label>
+                  <textarea placeholder="Enter Your Message" className="bg-primary placeholder:text-gray-400 px-4 py-2 border-gray-400 outline-none w-full text-secondary"></textarea>
                 </div>
               </div>
             </div>
             <button
-              className="bg-secondary px-7 py-2 text-center text-primary mt-14 mb-6 w-full"
+              className="bg-primary px-7 py-2 text-center text-secondary mt-14 mb-6 w-full rounded-xl hover:bg-secondary hover:text-primary transition-all border[2px solid #222]"
               type="submit"
             >
               Submit
